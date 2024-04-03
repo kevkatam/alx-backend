8c973b1d00524de81713ddebebd8db6637d6b8bf
kevkatam <kevkatam@gmail.com>
2024-04-03 11:25:46 +0300

    modified 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,11 +6,16 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """ basicache class that inherits from basecaching """
+    def __init__(self):
+        """init function initializes class from parent init method """
+        BaseCaching.__init__(self)
 
     def put(self, key, item):
         """assigns to the dictioanry self.cache_data the item value
            for the key
         """
+        if key is None or item is None:
+            return
         self.cache_data[key] = item
 
     def get(self, key):
