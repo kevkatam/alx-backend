d65f8f89138f4af0c89839a96f95239b9192081c
kevkatam <kevkatam@gmail.com>
2024-05-08 14:40:16 +0300

    adding 2-redis_op_async.js

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,30 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+}).on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+const get = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  const value = await get(schoolName).catch((err) => {
+    if (err) {
+      console.log(err);
+      throw err;
+ 
+    }
+  });
+  console.log(value);  
+}
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
