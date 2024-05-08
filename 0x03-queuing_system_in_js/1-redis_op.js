466ee2126c458259d3119759cc1bbc6d57482351
kevkatam <kevkatam@gmail.com>
2024-05-08 15:17:07 +0300

    adding 4-redis_advanced_op.js

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -13,7 +13,7 @@ function setNewSchool(schoolName, value) {
 }
 
 function displaySchoolValue(schoolName) {
-  client.get(schoolName, (err, value) => {;
+  client.get(schoolName, (err, value) => {
     console.log(value);
   });
 }
